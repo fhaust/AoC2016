10d33351b5d0fa74c8462d1077c8370d7071375b
Florian Hofmann <fho@f12n.de>
2016-12-05 10:25:59 +0100

    cryptohash is slightly faster

--- Day5.hs
+++ Day5.hs
@@ -1,36 +1,32 @@
-import Data.Digest.Pure.MD5
-import qualified Data.ByteString.Lazy.Char8 as BS
+import qualified Crypto.Hash.MD5 as MD5
+import qualified Data.ByteString.Char8 as BS
 
-import Data.List
 import qualified Data.IntMap.Strict as Map
 
-make  n key i = show . md5 $ BS.pack (key ++ show i)
-check = ("00000" `isPrefixOf`)
+import Data.List
+import Data.Hex
 
-check2 cs = (cs !! 5) `elem` "01234567"
+make  n key i = hex . MD5.hash $ BS.pack (key ++ show i)
 
-updateKey :: Map.IntMap Char -> String -> Map.IntMap Char
-updateKey m c = Map.insertWith (\_ b -> b) (read [c !! 5]) (c !! 6) m
+check     = (BS.pack "00000" `BS.isPrefixOf`)
+check2 cs = BS.index cs 5 `elem` "01234567"
 
-pass2 :: Map.IntMap Char -> String
-pass2 m = Map.elems $ Map.union m (Map.fromList (zip [0..] "________"))
+updateKey m c = Map.insertWith (\_ b -> b) (read [BS.index c 5]) (BS.index c 6) m
 
 main = do
 
     let key = "abbhdwsy"
-    {-let key = "abc"-}
 
     let hashes = filter check . map (make 5 key) $ [0..]
 
-    let result1 = map (!!5) . take 8 $ hashes
+    let result1 = map (`BS.index` 5) . take 8 $ hashes
 
     let hashes2 = filter check2 hashes
 
-    let result2 = map pass2
-                . takeWhile (\m -> Map.size m <= 8) 
+    let result2 = Map.elems . head
+                . dropWhile (\m -> Map.size m < 8)
                 . scanl' updateKey Map.empty
                 $ hashes2
 
     putStrLn $ "result part one: " ++ show result1
-    putStrLn $ "result part two: "
-    mapM_ print result2
+    putStrLn $ "result part two: " ++ show result2
